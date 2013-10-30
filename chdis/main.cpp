0a92c49a43241899287c82b7a36a0851f963d0c2
Colin Chen <beyond0924@gmail.com>
2013-10-30 12:43:34 +0800

    init project

---
+++ chdis/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  chdis
+//
+//  Created by ChenLin on 13-10-30.
+//  Copyright (c) 2013 GYGame. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
